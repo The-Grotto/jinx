5e407f8cd02f938381cde22c38dd83ade90e5e7f
Kyle Kartchner <kkartch@gmail.com>
2024-09-22 12:14:42 -0600

    feat: add Jinx.Doc and Jinx.DocServer modules

---
+++ lib/jinx/doc.ex
@@ -0,0 +1,47 @@
+defmodule Jinx.Doc do
+  @enforce_keys [:id, :ydoc]
+  defstruct [:id, :ydoc, connected_clients: []]
+
+  def new(doc_id) do
+    %Jinx.Doc{id: doc_id, ydoc: Yex.Doc.new()}
+  end
+
+  def apply_update(%Jinx.Doc{ydoc: ydoc} = doc, update) do
+    Yex.apply_update(ydoc, update)
+    doc
+  end
+
+  def get_doc_value(%Jinx.Doc{} = doc, element_type, element_name) do
+    case element_type do
+      :array -> {:ok, get_array_value(doc, element_name)}
+      :text -> {:ok, get_text_value(doc, element_name)}
+      :xml_fragment -> {:ok, get_xml_fragment_value(doc, element_name)}
+      :map -> {:ok, get_map_value(doc, element_name)}
+      _ -> {:error, "Unknown element type"}
+    end
+  end
+
+  def get_array_value(%Jinx.Doc{ydoc: ydoc}, name) do
+    ydoc
+    |> Yex.Doc.get_array(name)
+    |> Yex.Array.to_list()
+  end
+
+  def get_text_value(%Jinx.Doc{ydoc: ydoc}, name) do
+    ydoc
+    |> Yex.Doc.get_text(name)
+    |> Yex.Text.to_string()
+  end
+
+  def get_xml_fragment_value(%Jinx.Doc{ydoc: ydoc}, name) do
+    ydoc
+    |> Yex.Doc.get_xml_fragment(name)
+    |> Yex.XmlFragment.to_string()
+  end
+
+  def get_map_value(%Jinx.Doc{ydoc: ydoc}, name) do
+    ydoc
+    |> Yex.Doc.get_map(name)
+    |> Yex.Map.to_map()
+  end
+end
---
+++ test/jinx/doc_server_test.exs
@@ -0,0 +1,18 @@
+defmodule Jinx.DocServerTest do
+  use ExUnit.Case
+
+  test "creates a doc" do
+    {:ok, pid} = Jinx.DocServer.open_doc("123")
+
+    client_doc = Yex.Doc.new()
+    client_text = Yex.Doc.get_text(client_doc, "test_text")
+    Yex.Text.insert(client_text, 0, "test text input data")
+
+    {:ok, update} = Yex.encode_state_as_update(client_doc)
+
+    Jinx.DocServer.apply_update(pid, update)
+
+    assert Jinx.DocServer.get_doc_value(pid, :text, "test_text") ==
+             {:ok, "test text input data"}
+  end
+end
